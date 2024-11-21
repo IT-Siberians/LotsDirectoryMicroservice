4698eac96964f1c653f6d24650f67eedd01fdf27
Lietuonis <Lie2onis@gmail.com>
2024-11-21 19:02:19 +0700

    #GD-245 state: to verify work 17h

---
+++ LotDesignerMicroservice/Application/LotDesignerMicroservice.Application.Models/LotCard/UpdateLotCardModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LotDesignerMicroservice.Application.Models.LotCard
+{
+    public record class UpdateLotCardModel(
+        Guid id,
+        string Title,
+        string Description,
+        decimal StartingPrice,
+        decimal PriceStep,
+        decimal? RepurchasePrice,
+        int TradeDurationInHours,
+        IEnumerable<string> ImagesUrls);
+}
